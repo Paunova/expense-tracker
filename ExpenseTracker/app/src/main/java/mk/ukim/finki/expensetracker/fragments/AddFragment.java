249498dbda53111fdbb98dbcb2bfc31e024de771
spaunova <misspaunova@gmail.com>
2016-08-29 01:14:53 +0200

    Add sliding tab layout

---
+++ ExpenseTracker/app/src/main/java/mk/ukim/finki/expensetracker/fragments/AddFragment.java
@@ -0,0 +1,19 @@
+package mk.ukim.finki.expensetracker.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mk.ukim.finki.expensetracker.R;
+
+public class AddFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_layout,container,false);
+    }
+}

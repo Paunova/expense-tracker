c531439e0b44250f09efd2ec79d1d9146749cbf4
Angjel Kichukov <angelkicukov@hotmail.com>
2016-09-18 18:22:11 +0200

    Add Ploting Libs

--- ExpenseTracker/app/src/main/java/mk/ukim/finki/expensetracker/fragments/StatsFragment.java
+++ ExpenseTracker/app/src/main/java/mk/ukim/finki/expensetracker/fragments/StatsFragment.java
@@ -1,11 +1,20 @@
 package mk.ukim.finki.expensetracker.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 import mk.ukim.finki.expensetracker.R;
 
@@ -14,6 +23,31 @@ public class StatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        BarChart chart = new BarChart(getContext());
+
+        BarData data = new BarData(labels, dataset);
+        chart.setData(data);
+        chart.setDescription("# of times Alice called Bob");
         return inflater.inflate(R.layout.stats_layout,container,false);
     }
+
 }
